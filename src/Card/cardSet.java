ab0de57d45c3b6eaddb7c31c39b8b279dc8e377a
gilGAMEsh002 <2872752003@qq.com>
2022-11-30 15:13:42 +0800

    Initial commit

---
+++ src/Card/cardSet.java
@@ -0,0 +1,28 @@
+package Card;
+
+public class cardSet {
+    public String name;     //高排到皇家同花顺
+    public int weight = 1;      //1到10
+    public String getMax(card[] HandCard,card[] FiledCard,int stages){
+        int n = HandCard.length+FiledCard.length;//2到5到6到7
+        for(int i=0;i<n;i++){
+
+        }
+
+        return name;
+    }
+    /*找数字相同的牌有多少*/
+    /*
+    * 0     高牌
+    * 2     两对
+    * 3     三条
+    * 3+2   葫芦
+    * 4     四条
+    * */
+    public String same_card_calculate(card[] HandCard,card[] FiledCard,int stages){
+
+
+        return "YiDui";
+    }
+
+}

058ea94c3df81868a43451561ca590c205f47439
gilGAMEsh002 <2872752003@qq.com>
2022-12-03 12:48:56 +0800

    Initial commit

--- src/Card/card.java
+++ src/Card/card.java
@@ -1,9 +1,11 @@
 package Card;
 
 public class card {
-    int number ;              //2,3,4,5,6,7,8,9,10,11,12,13,14}
+    int number ;              //{2,3,4,5,6,7,8,9,10,11,12,13,14}
     int color ;    //1为♥,2为♦,3为♠,4为♣
     boolean exitCard = true;    //牌存在为:true
+    int cardWeight ;
+
 
     public card(int number, int color) {
         this.number = number;

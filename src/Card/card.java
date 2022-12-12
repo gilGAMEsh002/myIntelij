d7dfde4d9dbd91a7482818de43329f77f7058a2c
gilGAMEsh002 <2872752003@qq.com>
2022-12-12 19:03:33 +0800

    开始AI

--- src/Card/card.java
+++ src/Card/card.java
@@ -1,11 +1,11 @@
 package Card;
 
 public class card {
-    int number ;              //2,3,4,5,6,7,8,9,10,11,12,13,14}
-    int color ;    //1为♥,2为♦,3为♠,4为♣
+    public int number ;              //2,3,4,5,6,7,8,9,10,11,12,13,14}
+    public int color ;    //1为♥,2为♦,3为♠,4为♣
 
-    boolean exitCard = true;    //牌存在为:true
-    int cardWeight;
+    public boolean exitCard = true;    //牌存在为:true
+    public int cardWeight;
 
     public card(int number, int color) {
         this.number = number;
@@ -14,23 +14,24 @@ public class card {
 
     @Override
     public String toString() {
-        String value="";
-        switch (color){
-            case 1->value+="♥";
-            case 2->value+="♦";
-            case 3->value+="♠";
-            case 4->value+="♣";
-            default -> value+="";
-        }
-        switch (number){
-            case 11->value+="J";
-            case 12->value+="Q";
-            case 13->value+="K";
-            case 14->value+="A";
-            default -> value+=number;
-        }
-        return " "+value+" ";
+           String value="";
+//        switch (color){
+//            case 1->value+="♥";
+//            case 2->value+="♦";
+//            case 3->value+="♠";
+//            case 4->value+="♣";
+//            default -> value+="";
+//        }
+//        switch (number){
+//            case 11->value+="J";
+//            case 12->value+="Q";
+//            case 13->value+="K";
+//            case 14->value+="A";
+//            default -> value+=number;
+//        }
+//        return " "+value+" ";
 
+        return value+color+number;
     }
 
 
---
+++ src/Player/playerAI.java
@@ -0,0 +1,50 @@
+package Player;
+
+import Card.*;
+public class playerAI extends player {
+
+    //1.call 2.raise 3.fold
+    String preflop_choice="";
+    String flop_choice="";
+    String turn_choice="";
+    String river_choice="";
+
+    public String getPreflop_choice(playerAI player2){
+        card HandCard1 = this.handCard[0];
+        card HandCard2 = this.handCard[1];
+
+        /*如果两张牌都小于10且不成对,就应该Fold了.至于都小于10但成对的情况就跟住*/
+        if(HandCard1.number<10&&HandCard2.number<10&&HandCard2.number!=HandCard1.number){
+            System.out.println("AI选择3.fold");
+            return "3";
+        }
+
+        System.out.println("AI选择");
+
+        return "1";
+    }
+
+    public String getFlop_choice(playerAI player2){
+
+
+
+        return "1";
+    }
+
+
+    public String getTurn_choice_choice(playerAI player2){
+
+
+
+        return "1";
+    }
+
+    public String getRiver_choice_choice(playerAI player2){
+
+
+
+        return "1";
+    }
+
+
+}
